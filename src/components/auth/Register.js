59fdc70501124f49960bb8737c6d1e4fc562952f
Esto Triramdani N <estolagi@gmail.com>
2021-10-10 20:35:58 +0700

    Fix register user form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import {
   validateEmail,
   validateName,
@@ -9,7 +9,9 @@ import {
 import Gap from '../atoms/Gap';
 import { register } from '../../utils/helpers/auth';
 import { useRouter } from 'next/dist/client/router';
-import AuthAlert from '../atoms/Alert';
+import InputFormik from '../atoms/InputFormik';
+import Cookies from 'js-cookie';
+import { toast } from 'react-toastify';
 
 export const RegisterPage = () => {
   const router = useRouter();
@@ -17,15 +19,6 @@ export const RegisterPage = () => {
   const [isSuccess, setIsSuccess] = useState(true);
   return (
     <div>
-      {!isSuccess ? (
-        <AuthAlert
-          message="Register Failed. Username or email already registered."
-          type="danger"
-        />
-      ) : (
-        ''
-      )}
-
       <Formik
         initialValues={{
           name: '',
@@ -37,100 +30,67 @@ export const RegisterPage = () => {
         onSubmit={async (values, { setSubmitting }) => {
           setIsLoading(true);
           const createUser = await register(values);
-          await setIsLoading(false);
+          setIsLoading(false);
           if (createUser.token) {
-            // const protectedToken = safetyToken.protect(createUser.token);
-            window.localStorage.setItem('credentials', createUser.token);
+            toast.success('Register berhasil');
+            Cookies.set('credentials', btoa(createUser.token));
             window.localStorage.setItem(
               'user_info',
               JSON.stringify(createUser.user)
             );
-            router.push('/dashboard');
+            setTimeout(() => {
+              router.push('/auth/login');
+            }, 1000);
           } else {
-            setIsSuccess(false);
+            toast.error('Email sudah terdaftar.');
           }
           setSubmitting(false);
-        }}
-      >
+        }}>
         {({ errors, touched, isValidating }) => (
           <Form>
-            <div className="auth-form-group">
-              <label className="auth-form-control-label">Fullname</label>
-              <div className="auth-form-control-wrapper">
-                <div className="auth-form-control-icon">
-                  <i className={`bi bi-` + 'file-earmark-person-fill'}></i>
-                </div>
-                <Field
-                  name="name"
-                  validate={validateName}
-                  className="auth-form-control"
-                  autoComplete="off"
-                  placeholder="Your fullname"
-                />
-              </div>
-              {errors.name && touched.name && (
-                <div className="auth-form-message">{errors.name}</div>
-              )}
-            </div>
+            <InputFormik
+              errors={errors}
+              label={'Fullname'}
+              name={'name'}
+              placeholder={'Fullname'}
+              touched={touched}
+              type={'text'}
+              validator={validateName}
+              icon={'file-earmark-person-fill'}
+            />
             <Gap height={10} />
-            <div className="auth-form-group">
-              <label className="auth-form-control-label">Email</label>
-              <div className="auth-form-control-wrapper">
-                <div className="auth-form-control-icon">
-                  <i className={`bi bi-` + 'envelope'}></i>
-                </div>
-                <Field
-                  name="email"
-                  validate={validateEmail}
-                  className="auth-form-control"
-                  autoComplete="off"
-                  placeholder="Your email"
-                />
-              </div>
-              {errors.email && touched.email && (
-                <div className="auth-form-message">{errors.email}</div>
-              )}
-            </div>
+            <InputFormik
+              errors={errors}
+              label={'Email'}
+              name={'email'}
+              placeholder={'Your email'}
+              touched={touched}
+              type={'text'}
+              validator={validateEmail}
+              icon={'envelope-fill'}
+            />
             <Gap height={10} />
-            <div className="auth-form-group">
-              <label className="auth-form-control-label">Username</label>
-              <div className="auth-form-control-wrapper">
-                <div className="auth-form-control-icon">
-                  <i className={`bi bi-` + 'person-fill'}></i>
-                </div>
-                <Field
-                  name="username"
-                  validate={validateUsername}
-                  className="auth-form-control"
-                  autoComplete="off"
-                  placeholder="Your username"
-                />
-              </div>
-              {errors.username && touched.username && (
-                <div className="auth-form-message">{errors.username}</div>
-              )}
-            </div>
+            <InputFormik
+              errors={errors}
+              label={'Username'}
+              name={'username'}
+              placeholder={'Your username'}
+              touched={touched}
+              type={'text'}
+              validator={validateUsername}
+              icon={'person-fill'}
+            />
             <Gap height={10} />
-            <div className="auth-form-group">
-              <label className="auth-form-control-label">Password</label>
-              <div className="auth-form-control-wrapper">
-                <div className="auth-form-control-icon">
-                  <i className={`bi bi-` + 'key-fill'}></i>
-                </div>
-                <Field
-                  name="password"
-                  validate={validatePassword}
-                  className="auth-form-control"
-                  autoComplete="off"
-                  placeholder="Your password"
-                  type="password"
-                />
-              </div>
-              {errors.password && touched.password && (
-                <div className="auth-form-message">{errors.password}</div>
-              )}
-            </div>
-
+            <InputFormik
+              errors={errors}
+              label={'Password'}
+              name={'password'}
+              placeholder={'Password'}
+              touched={touched}
+              type={'password'}
+              validator={validatePassword}
+              icon={'key-fill'}
+            />
             <Gap height={20} />
             <button type="submit" className="button-secondary">
               {isLoading ? 'Please wait...' : 'Register'}
